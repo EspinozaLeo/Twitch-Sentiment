6f1b18895aa8b1fe78ea04daf31567511bfe8720
espinozal18 <espinozal18@students.ecu.edu>
2020-10-26 23:54:15 -0700

    Successful Twitch API authentication
    Author: Leo Espinoza
    Date: 10/26/2020
    Issue: #5
    
    letsrequest.py can now receive tokens to use with Twitch's API

--- logs/letsrequest.py
+++ logs/letsrequest.py
@@ -1,9 +1,11 @@
 # Author: Leo Espinoza
 
+import credentials
 import requests, json
 
-clientID = {'Client-ID': 'ENTER ID HERE'}
-clientSecret = {'Client-Secret': 'ENTER '}
+clientInfo = {'Client-ID': credentials.client_id,
+              'Client-Secret': credentials.client_secret}
+
 baseURL = 'https://api.twitch.tv/helix/'
 
 # get current top games
@@ -15,16 +17,21 @@ url = baseURL + "games/top"
 
 # getAuthenticated(cID) posts a request to the Twitch API and returns an
 # access token.
-def getAuthenticated(cID):
-    authURL = 'https://id.twitch.tv/oauth2/token?client_id=' + cID['Client-ID'] + '&client_secret=<enter value>&grant_type=client_credentials'
+def getAuthenticated(cInfo):
+    authURL = 'https://id.twitch.tv/oauth2/token?client_id=' + cInfo['Client-ID'] + '&client_secret=' + cInfo['Client-Secret'] + '&grant_type=client_credentials'
     resp = requests.post(authURL)
     parseResp = json.loads(resp.text)
 
     # DEBUG
     # formatJson(parseResp)
-
+    authenticateMe(parseResp['access_token'])
     return parseResp['access_token']
 
+def getStreamerInfo(streamerName):
+    getURL = baseURL + '/search/channels?query=' + streamerName
+    getResp = requests.get(getURL)
+    getParse = json.loads(getResp.text)
+    formatJson(getParse)
 
 # formatJson(jsonString) formats a json. This function is for
 # debugging purposes.
@@ -32,5 +39,14 @@ def formatJson(jsonString):
     formattedJson = json.dumps(jsonString, indent=2)
     print(formattedJson)
 
-accessToken = getAuthenticated(clientID)
-print(accessToken)
+def authenticateMe(aToken):
+    # authURL = 'https://api.twitch.tv/helix/'
+    authURL = 'https://api.twitch.tv/helix/search/channels?query=aceu'
+    myHeader = {'Client-ID': credentials.client_id,
+                'Authorization' : 'Bearer ' + aToken}
+    resp = requests.get(authURL, headers = myHeader)
+    getRespJson = json.loads(resp.text)
+    formatJson(getRespJson)
+
+accessToken = getAuthenticated(clientInfo)
+# getStreamerInfo()
