0bb4c3c43419654b53608f6d786f972392957f0f
espinozal18 <espinozal18@students.ecu.edu>
2020-11-04 23:31:57 -0800

    Added revoke token and safety measures on functions
    Author: Leo Espinoza
    Date 11/04/2020
    Email: chick3n13@gmail.com
    
    Revoking a token is now possible. Added safety checks to functions as
    well.

--- logs/letsrequest.py
+++ logs/letsrequest.py
@@ -8,33 +8,60 @@ clientInfo = {'Client-ID': credentials.client_id,
 
 baseURL = 'https://api.twitch.tv/helix/'
 
-# getAuthenticated(cID) authenticates the client and adds an oAuth token to
+# getAuthenticated(cID) authenticates the client and adds a bearer token to
 # clientInfo.
 def getAuthenticated(cInfo):
     authURL = 'https://id.twitch.tv/oauth2/token?client_id=' + cInfo['Client-ID'] + '&client_secret=' + cInfo['Client-Secret'] + '&grant_type=client_credentials'
     resp = requests.post(authURL)
     parseResp = json.loads(resp.text)
-    # The following line is for DEBUGGING purposes
+    # DEBUG
     # formatJson(parseResp)
     clientInfo['Authorization'] = 'Bearer ' + parseResp['access_token']
+    # DEBUG
+    # print('Successful authentication\n\n')
 
 # getStreamerInfo(streamerName) returns information about a streamer given
 # a streamer's name.
+# Requirements: clientInfo must contain an authenticated bearer token.
 def getStreamerInfo(streamerName):
-    streamerURL = baseURL + 'search/channels?query=' + streamerName
-    getResp = requests.get(streamerURL, headers = clientInfo)
-    print(getResp.status_code)
-    getParse = json.loads(getResp.text)
-    # print(getParse)
-    # formatJson(getParse)
-    print(getParse['data'][0])
-    # get current top games
+    if 'Authorization' in clientInfo:
+        streamerURL = baseURL + 'search/channels?query=' + streamerName
+        getResp = requests.get(streamerURL, headers = clientInfo)
+        getParse = json.loads(getResp.text)
+        # print(getParse)
+        # formatJson(getParse)
+        print(getParse['data'][0])
+        # get current top games
+    else:
+        print('You have not been authenticated.\n')
 
+# topStreams() prints the top games on Twitch at the time of request.
 def topStreams():
-    topGameURL = baseURL + "games/top"
-    topResp = requests.get(topGameURL, headers = clientInfo)
-    topParse = json.loads(topResp.text)
-    formatJson(topParse)
+    if 'Authorization' in clientInfo:
+        topGameURL = baseURL + 'games/top'
+        topResp = requests.get(topGameURL, headers = clientInfo)
+        topParse = json.loads(topResp.text)
+        formatJson(topParse)
+    else:
+        print('Not authenticated!')
+
+# revokeToken() revokes an authenticated token.
+# Requirements: clientInfo must contain an authenticated bearer token.
+def revokeToken():
+    if 'Authorization' in clientInfo:
+        revokeURL = ('https://id.twitch.tv/oauth2/revoke?client_id='
+               + clientInfo['Client-ID']
+               + '&token='
+               + clientInfo['Authorization'][7:])
+        revokeResp = requests.post(revokeURL)
+        print(revokeResp.text)
+        if revokeResp.status_code is 200:
+            print('Revoke successful.\n')
+            del clientInfo['Authorization']
+        else:
+            print(revokeResp.status_code)
+    else:
+        print('Token does not exist!')
 
 # formatJson(jsonString) formats a json. This function is for
 # debugging purposes. Prints readable json content.
@@ -47,4 +74,5 @@ streamer = 'nickmercs'
 
 getAuthenticated(clientInfo)
 getStreamerInfo(streamer)
-# getStreamerInfo()
+print('\n\nFinished getting streamer info')
+revokeToken()
